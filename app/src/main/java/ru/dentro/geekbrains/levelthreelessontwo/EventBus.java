0d32420319a8e795d43e375b15d2599dc88ea9ea
DentroApps <33389655+DentroApps@users.noreply.github.com>
2018-09-10 12:39:43 +0300

    horrible implementation of Event Bus

---
+++ app/src/main/java/ru/dentro/geekbrains/levelthreelessontwo/EventBus.java
@@ -0,0 +1,18 @@
+package ru.dentro.geekbrains.levelthreelessontwo;
+
+import io.reactivex.Observable;
+import io.reactivex.subjects.PublishSubject;
+
+public class EventBus {
+
+    private PublishSubject<Object> bus = PublishSubject.create();
+
+    public void send(Object o){
+        bus.onNext(o);
+    }
+
+    public Observable<Object> toObservable(){
+        return bus;
+    }
+
+}
--- app/src/main/java/ru/dentro/geekbrains/levelthreelessontwo/EventBusActivity.java
+++ app/src/main/java/ru/dentro/geekbrains/levelthreelessontwo/EventBusActivity.java
@@ -2,22 +2,100 @@ package ru.dentro.geekbrains.levelthreelessontwo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class EventBusActivity extends AppCompatActivity {
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+public class EventBusActivity extends AppCompatActivity{
 
     private TextView tvFirst, tvSecond;
     private EditText etFirst, etSecond;
+    private EventBus bus;
+    private Observer<Object> observer, observer2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_bus);
 
+        bus = new EventBus();
+
+        initUI();
+
+        bus.toObservable()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(observer);
+    }
+
+    private void initUI(){
+        observer = new Observer<Object>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(Object o) {
+                if (o.hashCode() == etFirst.getEditableText().hashCode())
+                    tvFirst.setText(o.toString());
+                else if (o.hashCode() == etSecond.getEditableText().hashCode())
+                    tvSecond.setText(o.toString());
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        };
+
         tvFirst = findViewById(R.id.tvFirst);
         tvSecond = findViewById(R.id.tvSecond);
         etFirst = findViewById(R.id.etFirst);
         etSecond = findViewById(R.id.etSecond);
+        etFirst.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                bus.send(s);
+            }
+        });
+        etSecond.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                bus.send(s);
+            }
+        });
     }
+
 }
